6233fc04b230bedf04a5d171891906db922de933
김민수 <minsu.kim1988@gmail.com>
2021-06-29 10:12:31 +0900

    28/June/2021 Equi Join, Outer Join, Self Join

--- Ex/Ex04.sql
+++ Ex/Ex04.sql
@@ -23,41 +23,77 @@ FROM
 WHERE
     em.department_id = de.department_id
     AND em.job_id = jo.job_id;
-
+    
+/*Outer join*/
 --LEFT OUTER JOIN
-select e.department_id, e.first_name, d.department_name
-from employees e left outer join departments d
-on e.department_id = d.department_id;
+select  em.employee_id,
+        em.first_name,
+        em.department_id,
+        de.department_name
+from employees em left outer join departments de
+on em.department_id = de.department_id;
 
-select e.department_id, e.first_name, d.department_name
-from employees e , departments d
-where e.department_id = d.department_id(+);
+select  em.employee_id,
+        em.first_name,
+        em.department_id,
+        de.department_name
+from employees em, departments de
+where em.department_id = de.department_id(+);
 
---Right outer Join
-select e.department_id, e.first_name, d.department_name
-from employees e right outer join departments d
-on e.department_id = d.department_id;
 
-select e.department_id, e.first_name, d.department_name
-from employees e , departments d
-where e.department_id(+) = d.department_id; 
+--right join
+select  em.employee_id,
+        em.first_name,
+        em.department_id,
+        de.department_name
+from employees em right outer join departments de
+on em.department_id = de.department_id;
 
---right outer join-> left outer join
-select em.emploee_id, em.first_name, em.department_id, de.department_name
-from departments de outer join employyes em 
-where em.;
+select  em.employee_id,
+        em.first_name,
+        em.department_id,
+        de.department_name
+from employees em, departments de
+where em.department_id(+) = de.department_id;
+
+--right join --> left join
+select  em.employee_id,
+        em.first_name,
+        em.department_id,
+        de.department_name
+from departments de left outer join employees em
+on de.department_id = em.department_id;
+
 
 --full outer join
-select e.department_id, e.first_name, d.department_name
-from employees e full outer join departments d
-on e.department_id = d.department_id;
+select  em.employee_id,
+        em.first_name,
+        em.department_id,
+        de.department_name 
+from employees em full outer join departments de
+on em.department_id = de.department_id;
 
---SELF join
-select    em.employee_id, 
-            em.first_name,
-            em.manager_id,
-            ma.employee_id,
-            ma.first_name,
-            ma.phone_number
+--full outer join + 기호 사용할 수 없다
+select  e.employee_id,
+        e.first_name,
+        e.department_id,
+        d.department_id
+from employees e, departments d
+where e.department_id(+) = d.department_id(+);
+
+--self join
+select  em.employee_id,
+        em.first_name,
+        em.phone_number,
+        em.manager_id,
+        em.department_id,
+        ma.employee_id,
+        ma.first_name,
+        ma.phone_number
 from employees em, employees ma
 where em.manager_id = ma.employee_id;
+
+--조인 컬럼을 사용시 항상 조심해서 사용
+select *
+from employees em , locations lo
+where em.salary = lo.location_id;
